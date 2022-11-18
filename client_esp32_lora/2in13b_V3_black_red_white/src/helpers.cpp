ecf8ddc1f7d15c6ccd1f81735c9f369e523dd4a5
Que0Le <lebanhat95@gmail.com>
2022-11-18 19:54:19 +0100

    switch to the newer protocol

---
+++ client_esp32_lora/2in13b_V3_black_red_white/src/helpers.cpp
@@ -0,0 +1,18 @@
+#include "helpers.h"
+
+void reset_vars(int *img_rqst_sent, int *flag, int *request_status, unsigned int *written_bytes,
+                unsigned char *shard_max_length, unsigned char *nbr_chunks,
+                unsigned int *session_id, int *current_chunk, int *current_shard)
+{
+    *img_rqst_sent = -1;
+    *flag = -1;
+    *request_status = -1;
+    *written_bytes = 0;
+    *shard_max_length = 0;
+    *nbr_chunks = 0;
+    *session_id = 0;
+    *current_chunk = -1;
+    *current_shard = -1;
+}
+
+void reset_buffs();
